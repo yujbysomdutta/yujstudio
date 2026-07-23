959abe9d17537829c6c6308ed32a9d2e6654ee08
yujbysomdutta <yujbysomdutta@gmail.com>
2026-07-23 23:49:05 +0530

    Set basename for BrowserRouter in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Home from "@/pages/Home";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter  basename="/yujstudio">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
